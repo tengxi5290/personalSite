1e444f6bb8aa65c5665d8929cfead31bbb38a5e1
tengxi5290 <13841845290@163.com>
2020-09-10 16:55:51 +0800

    Delete sw-register.js

--- sw-register.js
+++
@@ -1,6 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./service-worker.js').then((data)=>{
-        console.log(data)
-        console.log('sw注册成功')
-    }).catch(()=>{console.log('sw注册失败')})
-}
